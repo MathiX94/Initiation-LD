7af13ceda3a38db6914b22492f9f688db2b4b01d
MathiX94 <mathieu.brandao94@gmail.com>
2023-03-09 12:10:47 +0100

    update playform-button system + lv5 part1 done

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -5,32 +5,55 @@ using UnityEngine;
 public class ButtonManager : MonoBehaviour
 {
     public GameObject toMove;
-    public float toMoveSpeed;
+    public float toMoveSpeed = 1.0f;
     public bool isConstant = false;
+    public GameObject target;
 
     private Vector3 originPos;
     private Vector3 originButtonScale;
     private Vector3 targetPos;
     private bool triggered = false;
     private GameObject child;
+    private float timeOffset = 0;
+    private List<GameObject> touchingObjects = new();
+    private Vector3 position1;
 
     // Start is called before the first frame update
     void Start()
     {
-        targetPos = toMove.transform.Find("target").gameObject.transform.position;
+        targetPos = target.transform.position;
         originPos = toMove.transform.position;
         child = this.transform.Find("sprite").gameObject;
         originButtonScale = child.transform.localScale;
+        position1 = toMove.transform.position;
+    }
+
+    void platformActivated()
+    {
+        Vector3 oldPosition = toMove.transform.position;
+        toMove.transform.position = Vector3.Lerp(position1, targetPos, Mathf.PingPong((Time.time-timeOffset) * toMoveSpeed, 1.0f));
+
+        Vector3 displacement = toMove.transform.position - oldPosition;
+
+        foreach(GameObject obj in touchingObjects)
+        {
+            if (obj.transform.position.y <= toMove.transform.position.y) continue;
+
+            obj.transform.position += displacement;
+            if(obj.GetComponent<CharacterController2D>())
+                obj.GetComponent<CharacterController2D>().Translate(displacement);
+        }
+
     }
 
     void asConstant()
     {
         if (triggered) {
-            toMove.SendMessage("activatePlatform", true);
+            platformActivated();
             if (child.transform.localScale.y > 0)
                 child.transform.localScale -= new Vector3(0, 0.05f, 0);
         } else {
-            toMove.SendMessage("activatePlatform", false);
+            timeOffset += Time.deltaTime;
             if (child.transform.localScale.y < originButtonScale.y)
                 child.transform.localScale += new Vector3(0, 0.05f, 0);
         }
@@ -65,5 +88,15 @@ public class ButtonManager : MonoBehaviour
             triggered = !triggered;
             Debug.Log("not triggered");
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        touchingObjects.Add(collision.gameObject);
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        touchingObjects.Remove(collision.gameObject);
     }
 }
